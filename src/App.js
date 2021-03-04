4af85be8f0820c93bd0d8b75fd063221c631a626
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-04 13:32:22 +0600

    use axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import News from './components/News/News';
 
 function App() {
   const [articles,setArticles] = useState([]);
+  // useEffect(()=>{
+  //     fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=aa83b5d287e94483bdf47a0a22137b91`)
+  //     .then(res=>res.json())
+  //     .then(data =>setArticles(data.articles))
+  // },[])
   useEffect(()=>{
-      fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=aa83b5d287e94483bdf47a0a22137b91`)
-      .then(res=>res.json())
-      .then(data =>setArticles(data.articles))
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=aa83b5d287e94483bdf47a0a22137b91`
+    axios(url)
+    .then(data => setArticles(data.data.articles))
   },[])
   return (
      <div>
